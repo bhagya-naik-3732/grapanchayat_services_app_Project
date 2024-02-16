d05d6d35532f121d3e4120d47a9fce464dfcb54e
Bhagya Raju Naik <152792910+bhagya-naik-3732@users.noreply.github.com>
2024-02-16 12:42:06 +0530

    Add files via upload

---
+++ java/com/ayeshabhagya/grampanchayat/ui/complaint/ResolvedComplaintsFragment.java
@@ -0,0 +1,90 @@
+package com.ayeshabhagya.grampanchayat.ui.complaint;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.ayeshabhagya.grampanchayat.R;
+import com.ayeshabhagya.grampanchayat.data.model.Complaint;
+import com.ayeshabhagya.grampanchayat.databinding.FragmentFileComplaintBinding;
+import com.ayeshabhagya.grampanchayat.databinding.FragmentResolvedComplaintsBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResolvedComplaintsFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private ComplaintAdapter complaintAdapter;
+    private List<Complaint> complaintList;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(R.layout.fragment_resolved_complaints, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerUserComplaint);
+
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        complaintList = new ArrayList<>();
+        complaintAdapter = new ComplaintAdapter(getContext(), complaintList);
+        recyclerView.setAdapter(complaintAdapter);
+
+        readResolvedComplaints();
+
+        return view;
+    }
+
+
+    public void readResolvedComplaints() {
+        FirebaseDatabase.getInstance().getReference().child("Complaints").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                complaintList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Complaint complaint = dataSnapshot.getValue(Complaint.class);
+
+                    //add to list if email matches the current user
+
+                    FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                    if (complaint.getStatus().equals("Resolved")) {
+                        complaintList.add(complaint);
+                    }
+
+
+                }
+
+                complaintAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+
+
+}
---
+++ java/com/ayeshabhagya/grampanchayat/ui/home/HomeFragment.java
@@ -0,0 +1,75 @@
+package com.ayeshabhagya.grampanchayat.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.ayeshabhagya.grampanchayat.R;
+import com.ayeshabhagya.grampanchayat.data.model.News;
+import com.ayeshabhagya.grampanchayat.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private NewsAdapter newsAdapter;
+    private List<News> newsList;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate( R.layout.fragment_home, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerNews);
+
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+        newsList = new ArrayList<>();
+        newsAdapter = new NewsAdapter(getContext(), newsList);
+        recyclerView.setAdapter(newsAdapter);
+
+        readUsers();
+
+        return view;
+    }
+
+    public void readUsers() {
+        FirebaseDatabase.getInstance().getReference().child("News").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                newsList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    News news = dataSnapshot.getValue(News.class);
+                    newsList.add(news);
+                }
+
+                newsAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+}
